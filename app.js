d15568022eec4b55df6de022971cacc43573c5b5
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-24 00:47:25 -0500

    added button with functionality

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-let friends = ["Don", "Sherm", "Sean", "Ricky", "Chris"];
+document.addEventListener("DOMContentLoaded", function () {
+  let friends = ["Don", "Sherm", "Sean", "Ricky", "Chris"];
 
-for (let i = 0; i < friends.length; i++) {
-  console.log(friends[i]);
+  let button = document.createElement("button");
+  button.innerText = "Sing!";
+  document.body.appendChild(button);
 
-  for (let c = 99; c >= 1; c--) {
-    if (c == 1) {
-      console.log(
-        c +
-          " line of code in the file, " +
-          c +
-          " line of code; " +
-          friends[i] +
-          " strikes out, clears it all out, no more lines of code in the file"
-      );
-    } else {
-      console.log(
-        c +
-          " lines of code in the file, " +
-          c +
-          " lines of code; " +
-          friends[i] +
-          " strikes out, clears it all out, " +
-          (c - 1) +
-          " lines of code in the file"
-      );
+  button.addEventListener("click", function () {
+    for (let i = 0; i < friends.length; i++) {
+      console.log(friends[i]);
+
+      for (let c = 99; c >= 1; c--) {
+        if (c == 1) {
+          console.log(
+            c +
+              " line of code in the file, " +
+              c +
+              " line of code; " +
+              friends[i] +
+              " strikes out, clears it all out, no more lines of code in the file"
+          );
+        } else {
+          console.log(
+            c +
+              " lines of code in the file, " +
+              c +
+              " lines of code; " +
+              friends[i] +
+              " strikes out, clears it all out, " +
+              (c - 1) +
+              " lines of code in the file"
+          );
+        }
+      }
     }
-  }
-}
+  });
+});
