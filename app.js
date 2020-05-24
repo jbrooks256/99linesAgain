c5724dfc1ea946a37583fbf68963a9245e4e0d02
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-24 01:45:22 -0500

    replaced html body with dom elements

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   let friends = ["Don", "Sherm", "Sean", "Ricky", "Chris"];
 
+  let div = document.createElement("div");
+  div.className = "friend";
+
+  let Don = document.createElement("h3")
+  let Sherm = document.createElement("h3")
+  let Sean = document.createElement("h3")
+  let Ricky = document.createElement("h3")
+  let Chris = document.createElement("h3")
+
+  Don.innerText = "Don"
+  Sherm.innerText = "Sherm"
+  Sean.innerText = "Sean"
+  Ricky.innerText = "Ricky"
+  Chris.innerText = "Chris"
+
+  div.appendChild(Don);
+  div.appendChild(Sherm);
+  div.appendChild(Sean);
+  div.appendChild(Ricky);
+  div.appendChild(Chris);
+
+  document.body.appendChild(div);
+
   let button = document.createElement("button");
   button.innerText = "Sing!";
   document.body.appendChild(button);
